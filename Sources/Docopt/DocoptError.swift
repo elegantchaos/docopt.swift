c104a1eae35a8d12b98f117c4297c059a37ef73b
Sam Deane <sam@elegantchaos.com>
2018-12-11 17:24:52 +0000

    Error codes reflect actual errors.

--- Sources/Docopt/DocoptError.swift
+++ Sources/Docopt/DocoptError.swift
@@ -8,18 +8,24 @@
 
 import Foundation
 
+enum DocoptStatus: Int32 {
+    case unknownError = 0x1000
+    case languageError = 0x1001
+    case incorrectArguments = 0x1002
+}
+
 internal class DocoptError {
-    static let errorStatusCode: Int32 = 1000
-    
     var message: String
     var name: String
-   
+    var status: DocoptStatus
+    
     static var test: Bool = false
     static var errorMessage: String?
     
-    init (_ message: String, name: String) {
+    init (_ message: String, name: String, status: DocoptStatus = .unknownError) {
         self.message = message
         self.name = name
+        self.status = status
     }
     
     func raise(_ message: String? = nil) {
@@ -28,21 +34,21 @@ internal class DocoptError {
             DocoptError.errorMessage = msg
         } else {
             print(msg)
-            exit(DocoptError.errorStatusCode)
+            exit(status.rawValue)
         }
     }
 }
 
 internal class DocoptLanguageError: DocoptError {
     init (_ message: String = "Error in construction of usage-message by developer.") {
-        super.init(message, name: "DocoptLanguageError")
+        super.init(message, name: "DocoptLanguageError", status: .languageError)
     }
 }
 
 internal class DocoptExit: DocoptError {
     static var usage: String = ""
     init (_ message: String = "Exit in case user invoked program with incorrect arguments.") {
-        super.init(message, name: "DocoptExit")
+        super.init(message, name: "DocoptExit", status: .incorrectArguments)
     }
 
     override func raise(_ message: String? = nil) {
